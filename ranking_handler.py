0424697734836f6a4d7ad14e8395f04176eb6f40
Weronika Wiesiolek <wiesiolekweronika@gmail.com>
2020-12-31 01:31:56 +0100

    First ranking handler trial

--- ranking_handler.py
+++ ranking_handler.py
@@ -1,6 +1,9 @@
 def rank_user(current_level_dict, level_dicts):
+    rank_dict = {}
     for key in current_level_dict.keys():
         current_rank = 1
         for level_dict in level_dicts:
             if level_dict[key] > current_level_dict[key]:
                 current_rank += 1
+        rank_dict[key] = current_rank
+
